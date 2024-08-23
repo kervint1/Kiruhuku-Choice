806cfcde4e660eab6971398ed1d3189158fe4cc8
kervint1 <s1f102302124@iniad.org>
2024-08-23 20:02:07 +0900

    home基本画面構成

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,16 +6,66 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('着る服チョイス')),
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Text(
-          "着る服チョイス",
-          style: GoogleFonts.notoSansJp(
-              textStyle: const TextStyle(color: Colors.black, fontSize: 24)),
-        ),
+    //全体の割合0.94まで
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    final title = Container(
+      decoration: const BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.black, width: 2))),
+      height: screenHeight * 0.15,
+      width: double.infinity,
+      alignment: Alignment.center,
+      child: Text(
+        "着る服チョイス",
+        style: GoogleFonts.notoSansJp(
+            textStyle: const TextStyle(color: Colors.black, fontSize: 24)),
+        textAlign: TextAlign.center,
       ),
     );
+
+    final map_con = Container(
+      decoration: const BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.black, width: 2))),
+      height: screenHeight * 0.39,
+      width: double.infinity,
+      alignment: Alignment.center,
+      child: Text(
+        "Map",
+        style: GoogleFonts.notoSansJp(
+            textStyle: const TextStyle(color: Colors.black, fontSize: 24)),
+        textAlign: TextAlign.center,
+      ),
+    );
+
+    final register_con = Container(
+      decoration: const BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.black, width: 2))),
+      height: screenHeight * 0.3,
+      width: double.infinity,
+      alignment: Alignment.center,
+      child: Text(
+        "Registerbotton",
+        style: GoogleFonts.notoSansJp(
+            textStyle: const TextStyle(color: Colors.black, fontSize: 24)),
+        textAlign: TextAlign.center,
+      ),
+    );
+
+    final allObject = Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [title, map_con, register_con]);
+
+    final body = Container(
+      decoration: const BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.red, width: 2))),
+      child: allObject,
+    );
+
+    return Scaffold(
+        appBar: AppBar(
+            toolbarHeight: screenHeight * 0.1, title: const Text('着る服チョイス')),
+        backgroundColor: Colors.white,
+        body: body);
   }
 }
